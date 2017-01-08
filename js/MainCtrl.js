4b88089e44b3009e17d92242bff529028faaefbd
Nishant Koli <nishant.coolie@gmail.com>
2017-01-08 19:37:59 +0530

    Add files via upload

---
+++ js/MainCtrl.js
@@ -0,0 +1,10 @@
+myApp.controller('MainCtrl', ['$scope', function(scope) {
+
+	scope.appTitle = "Firth Fader";
+	scope.round = 5;
+	scope.username1 = "nishcooly";
+	scope.username2 = "sanmeet";
+	scope.message1 = "You cant see me!";
+	scope.message2 = "You can guess the gender, almost!";
+
+}]);
